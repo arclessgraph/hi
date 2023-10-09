57e0e72d93348ed6874349c41e7a8e7ad27d9007
arclessgraph <147434404+arclessgraph@users.noreply.github.com>
2023-10-09 11:01:04 -0700

    Add files via upload

---
+++ inttypesdemo.c
@@ -0,0 +1,39 @@
+/*
+	exploring
+		inttypes.h
+		stdint.h
+
+	http://www.opengroup.org/onlinepubs/9699919799/basedefs/inttypes.h.html
+
+   */
+
+#include <stdint.h>
+#include <inttypes.h>
+
+#include <wchar.h>
+
+int main(void)
+{
+	uint8_t		b = 0x3ef;
+	printf( "%d %d %d\n", 
+			b,
+		       	(signed char)b, 
+			sizeof(b)
+		);
+	
+	b = sizeof( struct {char a;} );
+	printf( "%d\n", b );
+
+	b = sizeof( char [5] ); 
+	printf( "%d\n", b );
+
+
+
+	uintmax_t	i = UINTMAX_MAX; // This type always exists.
+	wprintf(L"The largest integer value is %020" PRIxMAX "\n", i);
+
+	return 0;
+}
+
+
+
